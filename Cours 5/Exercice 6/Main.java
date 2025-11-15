13e90478f7258f21aa3b4951b3a2d50eddedc520
Naïké Dramé <naike.drame.dev@gmail.com>
2025-11-15 16:28:26 +0100

    correction ex 6 - 5 skipped

--- Cours 5/Exercice 6/Main.java
+++ Cours 5/Exercice 6/Main.java
@@ -4,6 +4,39 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         // Votre code doit être APRES de ce commentaire
+        // demander d'encoder les prix à l'user
+        
+        int total = 0;
+        int price;
+        int received = 0;
+        int change = 0;
+        
+        // enregistrer les prix
+        // tant que chiffre != 0, on continue à demander
+        do{
+            System.out.println("Veuillez encoder le prix de l'article : ");
+            price = readNextInt();
+            total += price;
+        }while(price != 0);
+        
+        // afficher montant total
+        System.out.println("Le montant total vaut : " + total + "€.");
+        // demander la somme donnée par le client
+        System.out.print("Montant reçu : ");
+        received = readNextInt();
+        
+        // rendre la monnaie  
+        // calculer la différence
+        change = received - total;
+        System.out.println("La somme à rendre vaut : " + change + "€.");
+        // cb de billets de 10
+        System.out.println("Billets de 10€ à rendre : " + (change/10));
+        change = change % 10;
+        // cb de pièces de 2
+        System.out.println("Pièces de 2€ à rendre : " + (change/2));
+        change = change % 2;
+        // cb de pièces de 1
+        System.out.println("Pièces de 1€ à rendre : " + change);
         
         // Votre code doit être AVANT de ce commentaire
     }
